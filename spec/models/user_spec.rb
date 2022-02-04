ce04a1e48dd1622609e767beb58c1ea677e0eaef
Micha Bernhard <mib70@pitt.edu>
2022-02-03 18:25:54 -0700

    Test model relationships and validations

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe User do
+  describe 'validations' do
+    it { should validate_presence_of :username }
+    it { should validate_presence_of :email }
+    it { should validate_presence_of :password }
+    it { should validate_uniqueness_of :email }
+    it { should validate_uniqueness_of :username }
+    it { should allow_value("test@test.com").for(:email) }
+    it { should_not allow_value("Inv4lid_").for(:email) }
+  end
+
+  describe 'relationships' do
+    it { should have_many :puzzles }
+    it { should have_many :requests }
+  end
+end
