bf48482d99f747ece24b22aeee7d2703f26ee4cd
Micha Bernhard <mib70@pitt.edu>
2022-02-03 18:26:09 -0700

    Adjust relationships and validations

--- app/models/puzzle.rb
+++ app/models/puzzle.rb
@@ -1,4 +1,4 @@
-class Puzzle << ActiveRecord
+class Puzzle < ApplicationRecord
   belongs_to :user
   has_many :requests
   validates_presence_of :image, :category, :piece_count, :availability
--- app/models/request.rb
+++ app/models/request.rb
@@ -1,4 +1,4 @@
-class Request << ApplicationRecord
+class Request < ApplicationRecord
   belongs_to :puzzle
   belongs_to :user
   validates_presence_of :status, :body
