73254ef8da1f10dfac6e0a2cfce7403a1720607c
Micha Bernhard <mib70@pitt.edu>
2022-02-04 14:12:28 -0700

    Create the endpoints for the puzzle api calls

--- app/controllers/api/v1/puzzles_controller.rb
+++ app/controllers/api/v1/puzzles_controller.rb
@@ -1,3 +1,9 @@
 class Api::V1::PuzzlesController < ApplicationController
+  def index
+    render json: PuzzleSerializer.new(Puzzle.where(availability: true))
+  end
 
+  def show
+    render json: PuzzleSerializer.new(Puzzle.find(params[:id]))
+  end
 end
