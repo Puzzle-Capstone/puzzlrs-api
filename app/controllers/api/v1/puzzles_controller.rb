360dcefde8f526fa47012f97b39a5c3a932b4d22
Micha Bernhard <mib70@pitt.edu>
2022-02-07 15:38:33 -0700

    Add puzzle post endpoint to controller and route

--- app/controllers/api/v1/puzzles_controller.rb
+++ app/controllers/api/v1/puzzles_controller.rb
@@ -6,4 +6,14 @@ class Api::V1::PuzzlesController < ApplicationController
   def show
     render json: PuzzleSerializer.new(Puzzle.find(params[:id]))
   end
+
+  def create
+    render json: PuzzleSerializer.new(Puzzle.create(puzzle_params))
+  end
+
+  private
+
+  def puzzle_params
+    params.permit(:id, :user_id, :image, :category, :piece_count, :missing_pieces, :availability, :quality, :original_price_point)
+  end
 end
