bdf0f1d98b99b2f2def498a9c17ba1686359794d
Micha Bernhard <mib70@pitt.edu>
2022-02-07 14:29:23 -0700

    delete body validation and test

--- spec/models/request_spec.rb
+++ spec/models/request_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 RSpec.describe Request do
   describe 'validations' do
     it { should validate_presence_of :status }
-    it { should validate_presence_of :body }
   end
 
   describe 'relationships' do
