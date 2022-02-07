001b269816bdc771ff7a724037ad1eeece274a7c
ChloeLL <chloell@protonmail.ch>
2022-02-07 15:27:10 -0700

    Add request create/update/delete

--- app/controllers/api/v1/requests_controller.rb
+++ app/controllers/api/v1/requests_controller.rb
@@ -1,3 +1,24 @@
 class Api::V1::RequestsController < ApplicationController
+  def create
+    request = Request.new(request_params[:user_id], request_params[:puzzle_id])
+    render json: RequestSerializer.new(request), status: 201
+  end
 
+  def update
+    request = Requests.find(params[:id])
+    request.update(status: params[:status])
+    render json: RequestSerializer.new(request)
+  end
+
+  def destroy
+    request = Requests.find(params[:id])
+
+    request.destroy
+  end
+
+  private
+
+  def request_params
+    params.permit(:user_id, :puzzle_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     namespace :v1 do
         resources :puzzles, only: [:index, :show]
         resources :users, only: :show
+        resources :requests, only: [:create, :update, :destroy]
     end
   end
 end
