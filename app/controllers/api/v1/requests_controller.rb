1b96d92713c7792f81dac1b038d2a4769a938b46
ChloeLL <chloell@protonmail.ch>
2022-02-08 11:23:27 -0700

    Update requests_controller.rb
    Add request create update and destroy methods

--- app/controllers/api/v1/requests_controller.rb
+++ app/controllers/api/v1/requests_controller.rb
@@ -1,17 +1,17 @@
 class Api::V1::RequestsController < ApplicationController
   def create
-    request = Request.new(request_params[:user_id], request_params[:puzzle_id])
-    render json: RequestSerializer.new(request), status: 201
+    request = Request.create!(user_id: request_params[:user_id], puzzle_id: request_params[:puzzle_id])
+    render json: RequestSerializer.new(request), response: 201
   end
 
   def update
-    request = Requests.find(params[:id])
+    request = Request.find(params[:id])
     request.update(status: params[:status])
     render json: RequestSerializer.new(request)
   end
 
   def destroy
-    request = Requests.find(params[:id])
+    request = Request.find(params[:id])
 
     request.destroy
   end
