55031b7c0886ce903a9b1a9c6e5244a70ce9da8c
Micha Bernhard <mib70@pitt.edu>
2022-02-14 13:35:13 -0700

    Create job for acceptance, rejection, notification

--- app/workers/mailer_worker.rb
+++ app/workers/mailer_worker.rb
@@ -1,7 +1,8 @@
 class MailerWorker
   include Sidekiq::Worker
+  sidekiq_options retry: false
 
-  def perform(user, puzzle)
-    RequestNotificationMailer.with(user: user, puzzle: puzzle).request_notification_email.deliver_now
+  def perform(user_id, puzzle_id)
+    RequestNotificationMailer.with(user: User.find(user_id.to_i), puzzle: Puzzle.find(puzzle_id.to_i)).request_notification_email.deliver_now
   end
 end
