5b19c72567e1276af0367647132937706511a3a4
Micha Bernhard <mib70@pitt.edu>
2022-02-15 16:36:45 -0700

    Change sidekiq config

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,12 +1,12 @@
-SIDEKIQ_REDIS_CONFIGURATION = {
-  url: ENV["REDIS_PROVIDER"], # if one assumes that REDIS_PROVIDER indirection is reliably present
-  ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } # we must trust Heroku and AWS here
-}
+# SIDEKIQ_REDIS_CONFIGURATION = {
+#   url: ENV["REDIS_PROVIDER"], # if one assumes that REDIS_PROVIDER indirection is reliably present
+#   ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } # we must trust Heroku and AWS here
+# }
 
 Sidekiq.configure_server do |config|
-  config.redis = SIDEKIQ_REDIS_CONFIGURATION
+  config.redis = { url: ENV['REDIS_PROVIDER'] }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = SIDEKIQ_REDIS_CONFIGURATION
+  config.redis = { url: ENV['REDIS_PROVIDER'] }
 end
