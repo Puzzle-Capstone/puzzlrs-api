f0aae6b655509bc3d44ad5b7a2b7f51f1fccdded
Micha Bernhard <mib70@pitt.edu>
2022-02-15 16:12:37 -0700

    delete comma

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,6 +1,6 @@
 SIDEKIQ_REDIS_CONFIGURATION = {
   url: ENV["REDIS_PROVIDER"], # if one assumes that REDIS_PROVIDER indirection is reliably present
-  ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }, # we must trust Heroku and AWS here
+  ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } # we must trust Heroku and AWS here
 }
 
 Sidekiq.configure_server do |config|
