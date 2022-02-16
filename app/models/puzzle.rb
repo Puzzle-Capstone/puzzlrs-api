78ac5faa7a70a915e3024ca42e08c8997488c5a8
ChloeLL <chloell@protonmail.ch>
2022-02-15 17:12:00 -0700

    Add dependency to requests

--- app/models/puzzle.rb
+++ app/models/puzzle.rb
@@ -1,5 +1,5 @@
 class Puzzle < ApplicationRecord
   belongs_to :user
-  has_many :requests
+  has_many :requests, :dependent => :destroy
   validates_presence_of :image, :category, :piece_count, :availability
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :puzzles
-  has_many :requests
+  has_many :requests, :dependent => :destroy
   validates_presence_of :username, :email
   validates_uniqueness_of :username
   validates :email, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
